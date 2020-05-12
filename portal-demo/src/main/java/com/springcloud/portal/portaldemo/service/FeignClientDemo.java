5ce5e89c8db32715c5b1a1a5e1f8fdbf01484c18
wlin <wanglin@ly-sky.com>
2020-05-12 09:56:44 +0800

    test分支第一次提交

--- portal-demo/src/main/java/com/springcloud/portal/portaldemo/service/FeignClientDemo.java
+++ portal-demo/src/main/java/com/springcloud/portal/portaldemo/service/FeignClientDemo.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @Author: hhj
+ * @Author: hhjhhj
  * @Date: Created in  2020/5/11
  */
 //@FeignClient(value="portalserver-demo",fallback=HFeignClient.class)
